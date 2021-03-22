dc0e8df7a82a233a31c7b00bee5c40e202fb59e4
nrhuizar <nrhuizar@gmail.com>
2021-03-22 16:01:45 -0700

    fixed import in SearchBooks

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
-import Auth from '../utils/Auth';
+import { Auth } from '../utils/Auth';
 import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 import { SAVE_BOOK } from '../utils/mutations';
