6c089f3c4f5871e4b2cab72c1b9175919549f6c4
nrhuizar <nrhuizar@gmail.com>
2021-03-22 18:34:33 -0700

    push to heroku

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
-import Auth from '../utils/Auth';
 import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 import { SAVE_BOOK } from '../utils/mutations';
 import { useMutation } from '@apollo/react-hooks';
+import Auth from '../utils/Auth';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
